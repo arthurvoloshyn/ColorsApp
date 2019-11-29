831dc61774ca5831dd33db2d162e0b078d25bdcd
Artur <grandfaraon93@gmail.com>
2019-11-29 13:50:37 +0200

    Updates

---
+++ precache-manifest.8ceb6de8c3760ef460383a4ab9889842.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/ColorsApp/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "89d047151c0d8925b513",
+    "url": "/ColorsApp/static/js/runtime~main.bf9388df.js"
+  },
+  {
+    "revision": "1effcd88647be2c15981",
+    "url": "/ColorsApp/static/js/main.b4077ae4.chunk.js"
+  },
+  {
+    "revision": "447bb36ac0eedb164100",
+    "url": "/ColorsApp/static/js/2.c9f83dcb.chunk.js"
+  },
+  {
+    "revision": "1effcd88647be2c15981",
+    "url": "/ColorsApp/static/css/main.ddf488c7.chunk.css"
+  },
+  {
+    "revision": "447bb36ac0eedb164100",
+    "url": "/ColorsApp/static/css/2.93bafb6d.chunk.css"
+  },
+  {
+    "revision": "96ca800f116dec0a333945d4fe20a33e",
+    "url": "/ColorsApp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ColorsApp/precache-manifest.8ceb6de8c3760ef460383a4ab9889842.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ColorsApp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
