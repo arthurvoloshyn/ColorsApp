989243022f567432fe4331f9663e698702f28db4
Artur <grandfaraon93@gmail.com>
2019-12-02 19:48:55 +0200

    add eslint coverage

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,9 +7,13 @@ export const randomColors = (palettes, colors, cb) => {
   while (isDuplicateColor) {
     rand = Math.floor(Math.random() * allColors.length);
     randomColor = allColors[rand];
+
+    /* eslint-disable no-loop-func */
     isDuplicateColor = colors.some(
       color => color.name === randomColor.name
     );
+    /* eslint-enable */
+
   };
 
   return cb(randomColor);
